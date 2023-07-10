29e110f64df15460e423138ff4b61638b0e6e097
Atty Cronin <antoniajcronin@gmail.com>
2023-07-10 12:01:40 +0100

    Rename Chuck Card Interface

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,11 +1,11 @@
 // 💡 You can import assets like images directly and use them in your components:
 import ChuckImage from '../assets/chuck_norris.jpg';
 
-interface ChuckCardProps {
+interface IChuckCardProps {
   greeting: string;
 }
 
-const ChuckCard: React.FC<ChuckCardProps> = ({ greeting }) => {
+const ChuckCard: React.FC<IChuckCardProps> = ({ greeting }) => {
   return (
     <>
       <h2>{greeting}</h2>
