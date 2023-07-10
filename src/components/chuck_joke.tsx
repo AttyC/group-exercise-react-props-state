4edfca7290a78abae8d0a485e89cfb5011b8d944
Atty Cronin <antoniajcronin@gmail.com>
2023-07-10 13:16:46 +0100

    Add filteredJokes to logic and display

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -4,12 +4,19 @@ interface ChuckJokeProps {
   jokes: Array<Joke>;
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = ({ jokes }) => (
-  <ul>
-    {jokes.map((joke: Joke) => (
-      <li key={joke.id}>😆 {joke.joke}</li>
-    ))}
-  </ul>
-);
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ jokes }) => {
+  const filteredJokes = jokes.filter((joke: Joke) => joke.id === 3);
+  return (
+    <>
+      <ul>
+        {jokes.map((joke: Joke) => (
+          <li key={joke.id}>😆 {joke.joke}</li>
+        ))}
+      </ul>
+      <h2>Filtered Jokes:</h2>
+      <p>😂 {filteredJokes[0].joke}</p>
+    </>
+  );
+};
 
 export default ChuckJoke;
